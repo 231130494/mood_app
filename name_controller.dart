08b17bf9e1f1a5048fbc27457776a109c12b036f
231130494 <167203628+231130494@users.noreply.github.com>
2025-06-21 22:00:35 +0700

    Update name_controller.dart

--- name_controller.dart
+++ name_controller.dart
@@ -1,18 +1,32 @@
+import 'package:flutter/material.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 
-class NameController {
-  static Future<void> saveUserName(String name) async {
+class NameController with ChangeNotifier {
+  String? _userName;
+
+  String? get userName => _userName;
+
+  NameController() {
+    _loadUserName();
+  }
+
+  Future<void> _loadUserName() async {
+    final prefs = await SharedPreferences.getInstance();
+    _userName = prefs.getString('userName');
+    notifyListeners();
+  }
+
+  Future<void> saveUserName(String name) async {
     final prefs = await SharedPreferences.getInstance();
     await prefs.setString('userName', name);
+    _userName = name;
+    notifyListeners();
   }
 
-  static Future<String?> getUserName() async {
-    final prefs = await SharedPreferences.getInstance();
-    return prefs.getString('userName');
-  }
-
-  static Future<void> clearUserName() async {
+  Future<void> clearUserName() async {
     final prefs = await SharedPreferences.getInstance();
     await prefs.remove('userName');
+    _userName = null;
+    notifyListeners();
   }
 }
