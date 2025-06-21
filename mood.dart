0613b0f8da9e6bafe16b914ff72585a7dff75ed2
231130494 <167203628+231130494@users.noreply.github.com>
2025-06-21 21:59:42 +0700

    Update mood.dart

--- mood.dart
+++ mood.dart
@@ -2,19 +2,19 @@ import 'current_mood.dart';
 import 'forecast.dart';
 
 class Mood {
-  final String user;
-  final Current current;
+  final String name;
+  final CurrentMood currentMood;
   final List<Forecast> forecast;
 
   Mood({
-    required this.user,
-    required this.current,
+    required this.name,
+    required this.currentMood,
     required this.forecast,
   });
 
   factory Mood.fromJson(Map<String, dynamic> json) => Mood(
-        user: json["user"],
-        current: Current.fromJson(json["current"]),
+        name: json["user"],
+        currentMood: CurrentMood.fromJson(json["current"]),
         forecast: List<Forecast>.from(
           json["forecast"].map((x) => Forecast.fromJson(x)),
         ),
